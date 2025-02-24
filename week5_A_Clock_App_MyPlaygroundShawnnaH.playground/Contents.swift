0c2b3a92036fb058f955f0b64f5d24a081e57441
Shawnna Hann <shawnna.inbox@gmail.com>
2025-02-23 22:59:40 -0600

    clock app first attempt failure

--- week5_A_Clock_App_MyPlaygroundShawnnaH.playground/Contents.swift
+++ week5_A_Clock_App_MyPlaygroundShawnnaH.playground/Contents.swift
@@ -1,6 +1,9 @@
-//Pseudocode Outline Exercise
-//Assignment: Create A Clock Recipe
-//Objective: Create a Clock App Recipe with Ingredients and Directions
+//Critical Thinking Exercise: Flow Exercise
+//Assignment: Design A Clock App
+//Objective: Design a Playground that simulates a working clock that keeps track of hours minutes and seconds
+
+
+//Original Clock Recipe Copy & Paste
 
 //Ingredients:
 //What You Need to Create A Clock App
@@ -25,3 +28,31 @@
     //Step 5: Create Constants like hour hand, minute hand, and second hand.
     //Step 6: Use a Loop statement to keep Time ticking
     //Step 7: Have an Experienced Coder create code in Swift
+
+//Define Variables
+var hours: Int = 24
+var minutes: Int = 58
+var seconds: Int = 59
+
+let currentTime = (hours, minutes, seconds)
+    if hours > 12 {
+        print("Current Time: \(hours - 12):\(minutes):\(seconds) PM")
+        print(" ")
+}
+    else {
+    print("\(hours):\(minutes):\(seconds) AM")
+
+}
+
+
+//Use of if, else if and else statement
+let deadlineTime = (24, 00, 00)
+
+if currentTime < deadlineTime {
+    print("You still have time to complete this assignment!")
+} else if currentTime == deadlineTime {
+    print("Your time is up!")
+} else {
+    print("💥 ERROR: CLOCK APP FAILURE💥 \n\nGROWTH MINDSET: ACTIVATED 💪 \nHELP MODULE: REQUESTED 🆘\nSECOND ATTEMPT: SCHEDULED FOR TOMORROW 🙏")
+}
+
