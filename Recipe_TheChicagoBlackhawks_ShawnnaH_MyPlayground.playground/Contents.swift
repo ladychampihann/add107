58491c43d0e1b0c3741f085ae19a6054bc3df89e
Shawnna Hann <shawnna.inbox@gmail.com>
2025-03-06 11:26:04 -0600

    intial Commit

---
+++ Recipe_TheChicagoBlackhawks_ShawnnaH_MyPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+//Pseudocode Outline Exercise
+//Assignment: Recipe The Chicago Blackhawks
+//Objective: For The Chicago Blackhawks Lab provide a recipe with Ingredients and Directions.
+
+
+//Ingredients:
+
+
+//Directions:
---
+++ week6_ChicagoBlackhawks_Collections_MyPlayground_ShawnnaH.playground/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+//Critical Thinking Lab: The Chicago Blackhawks
+
+//Week 6 Objectives:
+//The objectives are to understand what Arrays are and how they are used, comprehend what Dictionaries are and some of their benefits, and grasp what Tuples are and when they might be used. This will be followed by completing individual labs on Arrays, Dictionaries, and Tuples. After that, a lab combining all three topics will be completed, and finally, a Critical Thinking Exercise will be performed to apply the new knowledge of Collections in practical scenarios.
+
+//Assignment:
+//Source: https://www.nhl.com/blackhawks/roster
+
+//Create a dictionary of team members using their number as the key value
+var blackhawksRoster: [Int: String] =
+  [89:"Andreas Athanasiou",
+   98:"Connor Bedard",
+   59:"Tyler Bertuzzi",
+   28:"Colton Dach",
+   16:"Jason Dickinson",
+   8:"Ryan Donato",
+   
+  ]
+
+
+//Create as many other arrays as needed to store the rest of the info
+
+//Using control flow statements do the following;
+
+//Create a list of players sorted by age
+
+//Create a list of players sorted by country
+
+//Calculate the average age of the players
+
+//Calculate the average height of the players
+
+//Determine the month in which most of the players birthday falls
+
+//Upload a video explaining your working code
+
+//Upload your code to your Github repository and provide me a link
