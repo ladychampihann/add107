b8283a3d9a342a3ce464949148f4af3eb6110555
Shawnna Hann <shawnna.inbox@gmail.com>
2025-03-23 14:29:08 -0500

    week 9

---
+++ PrecisionAppProject_MyPlayground_ShawnnaH.playground/Contents.swift
@@ -0,0 +1,33 @@
+
+import SwiftUI
+
+// Sprint 1 - Precision Lead App - Final Project
+// Welcome Greeting
+let welcomeGreeting = "Welcome to the Precision Lead App!"
+print(welcomeGreeting)
+
+// Precision Lead - Game Day Quotes Feature
+// Quotes Array
+let gameDayQuotes = [
+    "\"The only way to do great work is to love what you do.\" - Steve Jobs",
+    "\"Nothing is impossible. The word itself says 'I'm possible!'\" - Audrey Hepburn",
+    "\"Believe you can and you're halfway there.\" - Theodore Roosevelt",
+    "\"Winners never quit and quitters never win.\" - Vince Lombardi",
+    "\"It's not the size of the dog in the fight, but the size in the fight of the dog.\" - Archie Griffin",
+    "\"The only thing we have to fear is fear itself.\" - Franklin D. Roosevelt",
+    "\"A champion is simply someone who did not give up when they wanted to.\" - Tom Landry",
+    "\"When you don’t give up, you cannot fail.\" - Adrian Peterson",
+    "\"The way a team plays as a whole determines its success.\" - Babe Ruth",
+    "\"Talent wins games, but teamwork and intelligence win championships.\" - Michael Jordan",
+    "\"In the end, it’s the effort that matters. The rest is beyond your control.\" - Maria Sharapova",
+    "\"No one ever drowned in sweat.\" - Lou Holtz",
+    "\"Coming together is a beginning. Keeping together is progress. Working together is success.\" - Henry Ford"
+    ]
+
+// Display Random Quote from the Array
+print(gameDayQuotes.randomElement() ?? "No quotes found")
+
+// Warm-Up Timer
+
+
+// Quick Game Day Notes
